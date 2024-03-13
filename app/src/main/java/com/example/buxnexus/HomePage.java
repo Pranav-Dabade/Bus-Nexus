c2b3d5571cdd23dce3bc3af14f811c558e3bbf9f
addybagwan47 <149690533+addybagwan47@users.noreply.github.com>
2024-03-13 11:49:33 +0530

    Update HomePage.java

--- app/src/main/java/com/example/buxnexus/HomePage.java
+++ app/src/main/java/com/example/buxnexus/HomePage.java
@@ -1,6 +1,8 @@
 package com.example.buxnexus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.cardview.widget.CardView;
 
 import android.annotation.SuppressLint;
@@ -9,23 +11,83 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
+
+import com.facebook.AccessToken;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginManager;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class HomePage extends AppCompatActivity {
 
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    AppCompatButton btn;
 
-    public CardView CVNewPAss,CVBusDetail,CVLocation,CVPassValidity;
+    public CardView CVNewPAss,CVBusDetail,CVLocation,CVPassValidity,CVUserDetail;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_home_page);
 
         CVNewPAss=findViewById(R.id.Card_new_Pass);
         CVBusDetail=findViewById(R.id.Card_Bus_Detail);
         CVLocation=findViewById(R.id.LOCATION);
         CVPassValidity=findViewById(R.id.Card_Pass_Validity);
+        //CVUserDetail = findViewById(R.id.User_detail);
+       btn = findViewById(R.id.sign_out);
 
+        gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build();
+        gsc = GoogleSignIn.getClient(this,gso);
+      //  AccessToken accessToken = AccessToken.getCurrentAccessToken();
+
+//        GraphRequest request = GraphRequest.newMeRequest(
+//                accessToken,
+//                new GraphRequest.GraphJSONObjectCallback() {
+//                    @Override
+//                    public void onCompleted(
+//                            JSONObject object,
+//                            GraphResponse response) {
+//                        try {
+//                            String fullName = object.getString("name");
+//
+//                        } catch (JSONException e) {
+//                          e.printStackTrace();
+//                        }
+//                    }
+//                });
+//        Bundle parameters = new Bundle();
+//        parameters.putString("fields", "id,name,link");
+//        request.setParameters(parameters);
+//        request.executeAsync();
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signout();
+                LoginManager.getInstance().logOut();
+               // startActivity(new Intent(HomePage.this, login.class));
+                Intent intent=new Intent(HomePage.this,login.class);
+                startActivity(intent);
+
+                finish();
+
+            }
+        });
 
         CVNewPAss.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,7 +101,7 @@ public class HomePage extends AppCompatActivity {
         CVLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(HomePage.this,location.class);
+                Intent intent=new Intent(HomePage.this,Location.class);
                 startActivity(intent);
 
             }
@@ -65,12 +127,35 @@ public class HomePage extends AppCompatActivity {
 
             }
         });
+        CVUserDetail=findViewById(R.id.User_detail);
+        CVUserDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(HomePage.this,user_info.class);
+                startActivity(intent);
+
+            }
+        });
 
 
 
 
 
     }
+    void signout()
+    {
+        gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                finish();
+//                startActivity(new Intent(HomePage.this,login.class));
+                Intent intent=new Intent(HomePage.this,login.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
 
 
 }
