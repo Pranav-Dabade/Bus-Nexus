8a6dfcd54fd6e6905f0fa9d5491067e6ff5e7045
addybagwan47 <149690533+addybagwan47@users.noreply.github.com>
2024-03-13 11:53:55 +0530

    Update location.java

--- app/src/main/java/com/example/buxnexus/location.java
+++ app/src/main/java/com/example/buxnexus/location.java
@@ -1,30 +1,49 @@
 package com.example.buxnexus;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import  com.google.android.gms.maps.OnMapReadyCallback;
 public class Location extends AppCompatActivity {
-    
+
 
     AppCompatButton btn;
 
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
 
-        btn=findViewById(R.id.location_Back_Btn);
+        btn=findViewById(R.id.Location_Back_Btn1);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Location.this,HomePage.class);
                 startActivity(intent);
+
+
             }
         });
+
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.maps);
+//        mapFragment.getMapAsync(this);
+//
+//
     }
+//
+//    @Override
+//    public void onMapReady(@NonNull GoogleMap googleMap) {
+//
+//    }
 }
