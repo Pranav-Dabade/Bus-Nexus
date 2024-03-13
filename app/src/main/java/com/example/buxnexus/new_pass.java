71456cd1b34336d0bafb5c981234a78d79e193e2
addybagwan47 <149690533+addybagwan47@users.noreply.github.com>
2024-03-13 11:57:10 +0530

    Update new_pass.java

--- app/src/main/java/com/example/buxnexus/new_pass.java
+++ app/src/main/java/com/example/buxnexus/new_pass.java
@@ -9,18 +9,27 @@ import android.view.View;
 
 public class new_pass extends AppCompatActivity {
 
-    AppCompatButton btnBack;
+    AppCompatButton btnBack,pay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_pass);
 
         btnBack=findViewById(R.id.New_Pass_Back_Btn);
+        pay = findViewById(R.id.New_Pass_Payment);
 
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(new_pass.this,HomePage.class);
+                startActivity(intent);
+            }
+        });
+        pay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(new_pass.this,payment.class);
                 startActivity(intent);
             }
         });
